2539090071c8f9fbcd5bc33347ed07cb9c33df23
Yijie Zou <yijiezou2004@gmail.com>
2024-02-23 13:34:42 -0700

    updated models

--- App/app/src/main/java/com/example/eventsigninapp/Attendee.java
+++ App/app/src/main/java/com/example/eventsigninapp/Attendee.java
@@ -1,11 +1,15 @@
 package com.example.eventsigninapp;
 
-public class Attendee {
+public class Attendee extends User{
     private String contact;
     //for events you have signed up for
     private Object events;
     private Object homepage;
     private Object picture;
+
+    protected Attendee(String id) {
+        super(id);
+    }
 
     public String getContact() {
         return contact;
