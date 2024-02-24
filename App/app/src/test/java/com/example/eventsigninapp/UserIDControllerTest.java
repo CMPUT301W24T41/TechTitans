4ba6fc6117a4b589a02d107d0f81dd61f49c3b10
Yijie Zou <yijiezou2004@gmail.com>
2024-02-23 17:06:07 -0700

    updated test comments

--- App/app/src/test/java/com/example/eventsigninapp/UserIDControllerTest.java
+++ App/app/src/test/java/com/example/eventsigninapp/UserIDControllerTest.java
@@ -35,43 +35,44 @@ public class UserIDControllerTest {
 
     @Test
     public void testGetUserID_existingUUID() {
-        // Arrange
+
         String existingUUID = "existingUUID";
+        //mock the process of acquiring a UUID
         when(mockContext.getSharedPreferences("ID", Context.MODE_PRIVATE)).thenReturn(mockPreferences);
         when(mockPreferences.getString("UUID_KEY", null)).thenReturn(existingUUID);
 
-        // Act
+        //Perform this using the function
         String result = userIDController.getUserID(mockContext);
 
-        // Assert
+        //check if we acquire the same result
         assertEquals(existingUUID, result);
     }
 
     @Test
     public void testGetUserID_generateUUID() {
-        // Arrange
+        //mock the results of creating and generating a UUID
         when(mockContext.getSharedPreferences("ID", Context.MODE_PRIVATE)).thenReturn(mockPreferences);
         when(mockPreferences.getString("UUID_KEY", null)).thenReturn(null);
         when(mockPreferences.edit()).thenReturn(mockEditor);
 
-        // Act
+        // check if the function performs the necessary action
         String result = userIDController.getUserID(mockContext);
 
-        // Assert
+        // see if the the result was saved to the preference
         verify(mockPreferences.edit()).putString("UUID_KEY", result);
     }
 
     @Test
     public void testSaveUUID() {
-        // Arrange
+        //mock the process of saving a result to the preference
         String uuid = "testUUID";
         when(mockContext.getSharedPreferences("ID", Context.MODE_PRIVATE)).thenReturn(mockPreferences);
         when(mockPreferences.edit()).thenReturn(mockEditor);
 
-        // Act
+        // attempt to do so with the function
         userIDController.saveUUID(mockContext, uuid);
 
-        // Assert
+        // see if the result is added then saved and committed
         verify(mockEditor).putString("UUID_KEY", uuid);
         verify(mockEditor).apply();
     }
