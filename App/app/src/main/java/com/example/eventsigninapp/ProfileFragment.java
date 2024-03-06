6c0c7abc740882f2bed48f1c2e5a75647dc7d258
CamBurr <burr.cameron@gmail.com>
2024-03-06 13:25:12 -0700

    Fix typo

--- App/app/src/main/java/com/example/eventsigninapp/ProfileFragment.java
+++ App/app/src/main/java/com/example/eventsigninapp/ProfileFragment.java
@@ -1,12 +1,11 @@
 package com.example.eventsigninapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,7 +56,7 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {g
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
