93d84ca20b281c4335a20a9a7317beb8b586e2b0
Kim Austria <kcaustria021@gmail.com>
2024-03-07 12:47:15 -0700

    Fixed merge conflicts

---
+++ App/app/src/main/java/com/example/eventsigninapp/AttendeeListActivity.java
@@ -0,0 +1,31 @@
+package com.example.eventsigninapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AttendeeListActivity extends AppCompatActivity {
+    TextView eventTitle;
+    ListView checkedInList;
+    ListView signedUpList;
+    Button switchToMapButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendee_list);
+
+        eventTitle = findViewById(R.id.event_title_text);
+        checkedInList = findViewById(R.id.checked_in_list);
+        signedUpList = findViewById(R.id.signed_up_list);
+        switchToMapButton = findViewById(R.id.button_to_map_view);
+
+        switchToMapButton.setOnClickListener(listener -> {
+            Intent goToMap = new Intent(AttendeeListActivity.this, MapActivity.class);
+        });
+    }
+}
---
+++ App/app/src/main/java/com/example/eventsigninapp/AttendeeListController.java
@@ -0,0 +1,57 @@
+package com.example.eventsigninapp;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class AttendeeListController {
+    DatabaseController dbController;
+    UserController userController;
+    ArrayList<User> signedUpUsers = new ArrayList<User>();
+    ArrayList<User> checkedInUsers = new ArrayList<User>();
+    public AttendeeListController() {
+        dbController = new DatabaseController();
+        userController = new UserController();
+    }
+
+    public AttendeeListController(DatabaseController dbController, UserController userController) {
+        this.dbController = dbController;
+        this.userController = userController;
+    }
+
+    public ArrayList<User> getSignedUpUsers() {
+        return signedUpUsers;
+    }
+
+    public ArrayList<User> getCheckedInUsers() {
+        return checkedInUsers;
+    }
+
+    /**
+     * This function puts a user into an array if they are checked into an event.
+     * @param userIDs the ID of the user that checked in
+     */
+    public void updateCheckedInUsers(ArrayList<?> userIDs) {
+        checkedInUsers.clear();
+        for (int i = 0; i < userIDs.size(); i++) {
+            userController.getUserFromFirestore((String) userIDs.get(i));
+            checkedInUsers.add(userController.getUser());
+        }
+    }
+
+    /**
+     * This function puts a user into an array if they are signed up to an event.
+     * @param userIDs the ID of the user that is signed up
+     */
+    public void updateSignedUpUsers(ArrayList<?> userIDs) {
+        signedUpUsers.clear();
+        for (int i = 0; i < userIDs.size(); i++) {
+            userController.getUserFromFirestore((String) userIDs.get(i));
+            signedUpUsers.add(userController.getUser());
+        }
+    }
+
+}
