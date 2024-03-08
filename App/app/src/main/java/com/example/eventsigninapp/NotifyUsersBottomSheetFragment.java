8e51a9408a6aa755552edbb3cde6005419abc4b8
Brendan Kunka <Brendankunka@outlook.com>
2024-03-08 15:32:08 -0700

    Added edit and notify buttons to event, visible only if the user is the event organizer

--- App/app/src/main/java/com/example/eventsigninapp/NotifyUsersBottomSheetFragment.java
+++ App/app/src/main/java/com/example/eventsigninapp/NotifyUsersBottomSheetFragment.java
@@ -1,10 +1,12 @@
 package com.example.eventsigninapp;
 
-import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +16,10 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class NotifyUsersBottomSheetFragment extends BottomSheetDialogFragment {
 
+    EditText message, title;
+    Spinner spinnerOptions;
+    Button sendButton;
+
     public NotifyUsersBottomSheetFragment() {
         // Required empty public constructor
     }
@@ -22,7 +28,15 @@ public class NotifyUsersBottomSheetFragment extends BottomSheetDialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notify_users_bottom_sheet, container, false);
+        View view = inflater.inflate(R.layout.fragment_notify_users_bottom_sheet, container, false);
+
+        // Initialize views
+        sendButton = view.findViewById(R.id.sendButton);
+        message = view.findViewById(R.id.editTextMessage);
+        title = view.findViewById(R.id.editTextTitle);
+        spinnerOptions = view.findViewById(R.id.spinnerFilter);
+
+        // Return the inflated view
+        return view;
     }
 }
-
