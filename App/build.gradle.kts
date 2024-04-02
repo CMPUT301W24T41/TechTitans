cab226a33870814f8dac79c76c62dd9095c277d1
CamBurr <burr.cameron@gmail.com>
2024-04-02 12:55:17 -0600

    Add argument to suppress warnings on build.

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -8,6 +8,10 @@ android {
     namespace = "com.example.eventsigninapp"
     compileSdk = 34
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     defaultConfig {
         applicationId = "com.example.eventsigninapp"
         minSdk = 27
